95ab978da56be37893ede84f71095ce665436cac
yuhuan <yuhuan0911@126.com>
2016-05-19 20:48:40 +0800

    :):):)

--- src/mini-query.js
+++ src/mini-query.js
@@ -1,37 +1,33 @@
-
 (function(w, d) {
 	'use strict';
+
 	function query(selector) {
 		var result = [];
-		if(d.querySelectorAll) {
+		if (d.querySelectorAll) {
 			var res = d.querySelectorAll(selector),
 				i = 0,
 				len = res.length;
-			for(; i < len; i++) {
+			for (; i < len; i++) {
 				result.push(res.item(i));
 			}
-
 		} else {
-
 			var sheet = d.styleSheets.length > 0 ? d.styleSheets[0] : d.createStyleSheet(),
 				elements = d.all,
-				len = elements.length;
+				len = elements.length,
 				result = [],
-				i = 0,
+				i = 0;
 
-			sheet.addRule(selector, 'summer:yu');
+				sheet.addRule(selector, 'summer:yu');
 
-			for(; i < len; i++) {
-				if(elements[i].currentStyle.summer) result.push(elements[i]);
+			for (; i < len; i++) {
+				if (elements[i].currentStyle.summer) result.push(elements[i]);
 			}
-
 			sheet.removeRule(0);
 		}
 		return result;
 
 	}
-	
+
 	window.$ = query;
 
 })(window, document);
-
